050cd42a3f58045e88650038598f0f3b01c9727e
Alex Adriano de Jesus  Souza <alexadriano.dev@gmail.com>
2023-03-23 19:33:34 -0300

    Criação dos card funcionarios e modal de cadastro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'employee-app';
-}
+   
+  public employees: Employee[] | undefined;
+  constructor(private employeeService: EmployeeService){
+
+  }
+
+  ngOnInit(){
+
+    this.getEmployees();
+    
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response
+      }, 
+      (error: HttpErrorResponse) => {
+         alert(error.message);
+
+      }
+    );
+  }
+
+
+  public onOpenModal(employee: Employee | null, mode: string): void{
+    const container = document.getElementById("main-container")
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if(mode === 'add'){
+        button.setAttribute('data-target', '#addEmployeeModal');
+        
+    }
+    if(mode === 'edit'){
+      button.setAttribute('data-target', '#updateEmployeeModal');
+    }
+    if(mode === 'delete'){
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+    container?.appendChild(button);
+    button.click();
+
+  }
+
+    public onAddEmloyee(addForm: NgForm): void {
+      document.getElementById('add-employee-form')?.click();
+      this.employeeService.addEmployee(addForm.value).subscribe(
+        (response: Employee) => {
+          console.log(response);
+          this.getEmployees();
+          addForm.reset();
+        },
+        (error: HttpErrorResponse) => {
+          alert(error.message);
+          addForm.reset();
+        }
+      );
+    }
+
+
+  } 
+
